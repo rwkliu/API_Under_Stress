96e9fe0e5dfe6700bc6d06e832ad52aa4a6a2164
Prakash Shrestha <prakashroj@gmail.com>
2024-04-11 21:01:53 -0400

    added test for successful creation of warrior

--- app/test_api.py
+++ app/test_api.py
@@ -8,9 +8,33 @@ def client():
     with app.test_client() as client:
         yield client 
 
+
 def test_default(client):
     response = client.get('/') 
-    print(response.status_code)
-    print(response.data)
+    # print(response.status_code)
+    # print(response.data)
     assert response.status_code == 200 
-    assert response.data.decode('utf-8') == "Welcome to API Under Stress!"
+    assert response.data.decode('utf-8') == "Welcome to API Under Stress!" 
+    
+
+
+def test_create_warrior(client):
+    data = {
+        "id": None,
+        "name": "Red Sky",
+        "dob": "1985-02-14",
+        "fight_skills": ["kungfu", "taekwondo"]
+    }
+    response = client.post('/warrior', data=json.dumps(data), content_type='application/json')
+    assert response.status_code == 201
+
+    response_data = response.json
+    # print("Response data:", response_data)
+
+    assert 'id' in response_data
+    data['id'] = response_data['id']
+    assert data['id'] is not None
+
+    assert 'message' in response_data
+    assert response_data['message'] == 'Warrior created successfully'
+
