bcea56ff6a749f47f976b965a74239c1a70a3e6f
Ricky Liu <114320485+rwkliu@users.noreply.github.com>
2024-04-28 14:04:29 -0700

    Change API to work with gatling_testing_v1 (#40)
    * Update gitignore to ignore gatling_testing_v1 directory
    
    * Change Nginx port to 9999
    
    * Revise GET Request by id to cache the view function
    
    * Revise GET warrior by search term to cache the view function
    
    * Add new warrior's details to POST return response; disable dictionary key sorting
    
    * Add checks for 0 or more than 20 fight skills
    
    * Add custom cache key definition to cache the search term
    
    * Add check for name length
    
    * Add check to see if any fight skills are longer than 250 characters long
    
    * Add check for the total sum of the fight skill character lengths not exceeding max length
    
    * Revise /warrior POST request
    convert fight_skills list into a comma separated string
    Set max_skills_length to 5019 to include the extra commas
    Generate the uuid
    Insert it with the other values in the insert query
    
    * Add condition in cache decorator to prevent caching empty path/query parameters
    
    * Revise warrior db
    Set fight_skills length to 5019
    Remove all warriors values
    
    * Add location header to POST request response
    
    * Change link to the Gatling simulation script

--- app/api.py
+++ app/api.py
@@ -1,9 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from flask_caching import Cache
 import mysql.connector
-import uuid
+import uuid, json
 
 app = Flask(__name__)
+
+# App config settings
+app.json.sort_keys = False
 
 # Configure the redis cache
 app.config["CACHE_TYPE"] = "RedisCache"
@@ -38,7 +41,7 @@ def create_warrior():
     db = connect_to_db()
     data = request.json
 
-    # Perform input validation
+    # Check name, dob, and fight_skills keys are in the request body
     if "name" not in data or "dob" not in data or "fight_skills" not in data:
         return jsonify({"message": "Bad Request - Missing required fields"}), 400
 
@@ -47,14 +50,45 @@ def create_warrior():
     dob = data.get("dob")
     fight_skills = data.get("fight_skills")
 
+    # Check the name is more than 100 characters
+    if len(name) > 100:
+        return jsonify({"message": "Bad Request - name is too long"}), 400
+    # Check for empty skills
+    if fight_skills == None:
+        return jsonify({"message": "Bad Request - fight_skills cannot be empty"}), 400
+    # Check for more than 20 fight skills
+    if len(fight_skills) > 20:
+        return (
+            jsonify(
+                {
+                    "message": "Bad Request - fight_skills cannot cannot have more than 20 skills"
+                }
+            ),
+            400,
+        )
+    # Check for skills that are more than 250 characters
+    if any(len(fight_skill) > 250 for fight_skill in fight_skills):
+        return jsonify({"message": "Bad Request - a fight skill name is too long"}), 400
+    # Check for total sum of the fight skill character lengths doesn't exceed max length
+    max_skills_length = 5019
+    if sum([len(fight_skill) for fight_skill in fight_skills]) > max_skills_length:
+        return jsonify({"message": "Bad Request - fight skills length exceeded"}), 400
+
+    fight_skills_list_string = ",".join(fight_skills)
     cursor = db.cursor()
     sql = "INSERT INTO warriors (id, name, dob, fight_skills) VALUES (%s, %s, %s, %s)"
-    values = (id, name, dob, str(fight_skills))
+    values = (id, name, dob, fight_skills_list_string)
 
     try:
         cursor.execute(sql, values)
         db.commit()
-        return jsonify({"message": "Warrior created successfully", "id": id}), 201
+        resp = Response(
+            response=json.dumps({"message": "Warrior created successfully"}),
+            status=201,
+        )
+        resp.headers["location"] = "/warrior/" + id
+
+        return resp
     except Exception as e:
         print("Error creating warrior:", e)
         db.rollback()
@@ -65,7 +99,7 @@ def create_warrior():
 
 # GET request that searches the database for entries that matches the given id
 @app.route("/warrior/<id>", methods=["GET"])
-@cache.cached(timeout=60, key_prefix="warrior_id")
+@cache.cached(timeout=60)
 def get_warrior(id):
     db = connect_to_db()
     cursor = db.cursor()
@@ -86,12 +120,21 @@ def get_warrior(id):
         cursor.close()
 
 
-# GET request that searches the databases for entries that matches the given name
+def get_search_term():
+    search_term = request.args.get("t")
+    return search_term
+
+
+def search_term_none():
+    return request.args.get("t") == None
+
+
+# GET request that searches for entries that matches the given search term
 @app.route("/warrior", methods=["GET"])
-@cache.cached(timeout=60, key_prefix="warrior_att")
+@cache.cached(timeout=60, make_cache_key=get_search_term, unless=search_term_none)
 def search_warriors():
     db = connect_to_db()
-    search_term = request.args.get("t")
+    search_term = get_search_term()
     if not search_term:
         return jsonify({"message": "Bad Request"}), 400
 
