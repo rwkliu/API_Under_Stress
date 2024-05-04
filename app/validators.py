4e1f7cbcdf28f4c4eaf3263dfa5ad4284f9b93f1
rwkliu <114320485+rwkliu@users.noreply.github.com>
2024-05-04 08:40:38 -0700

    Add function to check fight_skills for duplicate elements

--- app/validators.py
+++ app/validators.py
@@ -29,12 +29,20 @@ def is_valid_skill(skill):
     return valid_skills[skill]
 
 
+def contains_duplicates(skills):
+    skills_set = set(skills)
+    return len(skills) != len(skills_set)
+
+
 def validate_fight_skills(fight_skills):
     if not fight_skills:
         return "Bad Request - Fight skills cannot be empty"
+
     try:
         if len(fight_skills) > 20:
             return "Bad Request - Fight skills cannot have more than 20 skills"
+        if contains_duplicates(fight_skills):
+            return "Bad Request - Fight skills contains duplicates"
         if not all(is_valid_skill(skill) for skill in fight_skills):
             return "Bad Request - Invalid fight skill"
     except TypeError:
